fcf39f7c8ee14abfb112934287598d4cf0e04ef1
Piyush Kumar <64326511+Dwivedi1823@users.noreply.github.com>
2024-03-09 22:11:37 +0530

    Time: 103 ms (5.42%) | Memory: 63.6 MB (5.01%) - LeetSync

--- 189-rotate-array/rotate-array.js
+++ 189-rotate-array/rotate-array.js
@@ -14,10 +14,20 @@ function rev(arr, start, end) {
 var rotate = function(nums, k) {
     n = nums.length;
     k = k%n;
-    rev(nums, 0, n-k);
-    console.log(nums);
-    rev(nums, n-k,n);
-    console.log(nums);
-    rev(nums, 0, n);
+    // rev(nums, 0, n-k);
+    // console.log(nums);
+    // rev(nums, n-k,n);
+    // console.log(nums);
+    // rev(nums, 0, n);
+    for(let i = 0; i < Math.floor((n-k)/2); ++i){
+        [nums[i], nums[n-k-i-1]] = [nums[n-k-i-1], nums[i]];
+    }
+    for(let i = n-k, j = 1; j <= Math.floor(k/2); ++i, ++j){
+        [nums[i], nums[n-j]] = [nums[n-j], nums[i]];
+    }
 
+    for(let i = 0; i < Math.floor(n/2); ++i){
+        [nums[i], nums[n-i-1]] = [nums[n-i-1], nums[i]]
+    }
+    console.log(nums);
 };
