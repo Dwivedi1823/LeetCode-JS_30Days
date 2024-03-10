b2ab5348631c3f12949d42ad54dd6dd233b87333
Piyush Kumar <64326511+Dwivedi1823@users.noreply.github.com>
2024-03-10 22:23:57 +0530

    Time: 48 ms (83.14%) | Memory: 48.5 MB (74.93%) - LeetSync

--- 27-remove-element/remove-element.js
+++ 27-remove-element/remove-element.js
@@ -4,33 +4,14 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    let i = 0, j = nums.length;
-    let res = 0;
-    // while(i < j){
-    //     if(nums[i] === val && nums[j] === val){
-    //         j--;
-    //         [nums[i], nums[j]] = [nums[j], nums[i]];
-    //         res++;
-    //     }
-    //     else if(nums[i] === val && nums[j] !== val){
-    //         [nums[i], nums[j]] = [nums[j], nums[i]];
-    //         res++;
-    //     }
-    //     i++;
-    //     j--;
-    // }
-    let aux = [];
-    for(let p = 0; p < j; ++p){
-        if(nums[p] === val){
-            res++;
-            continue;
-        }
-        else{aux.push(nums[p]);}
-    }
+  let i = 0; // i points to the position where the next non-val element should be placed
 
-    for(let w = 0; w < aux.length; ++w){
-        nums[w] = aux[w];
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== val) {
+      nums[i] = nums[j]; // copy the non-val element to the correct position
+      i++;
     }
-    console.log(res);
-    return j - res;
+  }
+
+  return i; // i represents the number of elements not equal to val
 };
