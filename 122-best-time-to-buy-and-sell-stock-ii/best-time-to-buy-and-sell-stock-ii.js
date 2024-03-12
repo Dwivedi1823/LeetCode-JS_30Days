d276cd16f7be2b1587ddcce758edfc8ad5ae74d6
Piyush Kumar <64326511+Dwivedi1823@users.noreply.github.com>
2024-03-12 16:18:15 +0530

    Time: 55 ms (58.89%) | Memory: 51.3 MB (7.54%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
@@ -3,14 +3,20 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let res = 0; 
-    let buy = prices[0];
+    // let res = 0; 
+    // let buy = prices[0];
+    // for(let i = 1; i < prices.length; ++i){
+    //     if(buy > prices[i]) buy = prices[i];
+    //     else if(prices[i]-buy > 0) {
+    //         res += (prices[i] - buy);
+    //         buy = prices[i];
+    //     }
+    // }
+    // return res;
+
+    let res = [0];
     for(let i = 1; i < prices.length; ++i){
-        if(buy > prices[i]) buy = prices[i];
-        else if(prices[i]-buy > 0) {
-            res += (prices[i] - buy);
-            buy = prices[i];
-        }
+        res[i] = res[i-1] + Math.max(prices[i] - prices[i-1], 0);
     }
-    return res;
+    return res[prices.length - 1];
 };
