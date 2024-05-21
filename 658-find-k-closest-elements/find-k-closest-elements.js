66ede1876a2fead1f13c0171566a4579c2c2e9bd
Piyush Kumar <64326511+Dwivedi1823@users.noreply.github.com>
2024-05-21 11:16:44 +0530

    Time: 90 ms (25.88%) | Memory: 56.7 MB (36.87%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.js
+++ 658-find-k-closest-elements/find-k-closest-elements.js
@@ -5,23 +5,25 @@
  * @return {number[]}
  */
 var findClosestElements = function(arr, k, x) {
-    let pq = [];
-    let res = [];
-    let n = arr.length;
+    // let pq = [];
+    // let res = [];
+    // let n = arr.length;
+    // test = new MaxPriorityQueue();
+    // for(let i = 0; i < k; ++i){
+    //     pq.push([Math.abs(arr[i] - x), i]);
+    // }
+    // for(let i = k; i < n; ++i){
+    //     let diff = Math.abs(arr[i] - x);
+    //     if(diff >= pq[0][0]) continue;
+    //     pq.shift();
+    //     pq.push([diff, i]);
+    // }
 
-    for(let i = 0; i < k; ++i){
-        pq.push([Math.abs(arr[i] - x), i]);
-    }
-    for(let i = k; i < n; ++i){
-        let diff = Math.abs(arr[i] - x);
-        if(diff >= pq[0][0]) continue;
-        pq.shift();
-        pq.push([diff, i]);
-    }
-
-    while(pq.length > 0){
-        res.push(arr[pq[0][1]]);
-        pq.shift();
-    }
-    return res;
+    // while(pq.length > 0){
+    //     res.push(arr[pq[0][1]]);
+    //     pq.shift();
+    // }
+    // return res;
+    arr.sort((a,b)=> Math.abs(a - x) - Math.abs(b - x));
+    return arr.slice(0,k).sort((a,b)=>a-b);
 };
